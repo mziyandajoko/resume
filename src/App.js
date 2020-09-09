bc041ce4b719b9c03b540053fd20c18a009fad06
Mziyanda Joko <mziyandajoko@Mziyandas-MacBook-Pro.local>
2020-09-09 15:55:27 +0200

    removes 404 page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Work from "./pages/Work";
 import ExpectIn from "./pages/ExpectIn";
-import Page404 from "./pages/Page404";
 import NavigationBar from "./components/NavigationBar";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
@@ -17,7 +16,6 @@ function App() {
             <Route exact path="/" component={About} />
             <Route path="/pages/Work" component={Work} />
             <Route path="/pages/ExpectIn" component={ExpectIn} />
-            <Route component={Page404} />
           </Switch>
         </Router>
       </Layout>
